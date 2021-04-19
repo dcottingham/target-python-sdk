ba3b7a100c395bf1696978440614ef6615163d48
Don Cottingham <cottingh@adobe.com>
2021-04-19 12:53:20 -0700

    fix TLD and subdomain parsing by using 3rd party lib

--- target_decisioning_engine/tests/test_geo_provider.py
+++ target_decisioning_engine/tests/test_geo_provider.py
@@ -7,7 +7,7 @@
 # the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
 # OF ANY KIND, either express or implied. See the License for the specific language
 # governing permissions and limitations under the License.
-"""Test cases for taraget_decisioning_engine.geo_provider module"""
+"""Test cases for target_decisioning_engine.geo_provider module"""
 try:
     from unittest.mock import patch
 except ImportError:
